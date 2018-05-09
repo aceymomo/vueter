cf3f494f6b2e2cbe5f60e95004b9c11256bd40f1
aceymomo <kobezhang333@vip.qq.com>
2018-05-09 20:11:56 +0800

    indexSwiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'HomeSwiper',
+    data(){
+      return{
+        swiperOption:{
+          pagination:'.swiper-pagination',
+          loop:true
+        },
+        swiperList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1805/44/70b819ab2b3c02.jpg_750x200_fe9147c8.jpg'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/34/db1fe32d3ca04b02.jpg_750x200_f541a10c.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background :#fff
+.wrapper
+  overflow :hidden
+  width:100%
+  height :0
+  padding-bottom :26.67%
+  background :#eee
+  .swiper-img
+    width:100%
+</style>
