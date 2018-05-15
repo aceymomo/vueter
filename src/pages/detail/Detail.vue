5cd310ed4432d239e46d588e490ca9fb652bc7fb
aceymomo <kobezhang333@vip.qq.com>
2018-05-15 19:40:47 +0800

    detailHeader

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="home"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import DetailBanner from '@/pages/detail/components/banner'
+import DetailHeader from '@/pages/detail/components/Header'
   export default {
     name:"Detail",
     data() {
@@ -15,12 +18,13 @@ import DetailBanner from '@/pages/detail/components/banner'
 
     },
     components: {
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     }
   }
 </script>
 
 <style scoped lang="stylus">
-
- 
+.home
+  height 50rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <router-link to="/">
+      <div class="abs" v-show="showAbs">
+        <span class="iconfont">&#xe624;</span>
+      </div>
+    </router-link>
+    <transition name="fade">
+      <div class="fixed" v-show="!showAbs">
+        <span class="iconfont">&#xe624;</span>
+        宁波方特东方神话
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        showAbs:true
+      }
+
+    },
+    components: {
+
+    },
+    methods:{
+      handleScroll(){
+        const top = document.documentElement.scrollTop
+        if(top > 60){
+          this.showAbs = false
+        }else{
+          this.showAbs = true
+        }
+      }
+    },
+    activated(){
+      window.addEventListener('scroll',this.handleScroll)
+    },
+    deactivated(){
+      window.removeEventListener('scroll',this.handleScroll)
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+.fade-enter
+  opacity 0
+.fade-enter-active,.fade-leave-active
+  transition all 0.5s
+.fade-leave-to
+  opacity 0
+.abs
+  z-index 2
+  position absolute
+  left .2rem
+  top .04rem
+  width .8rem
+  height .8rem
+  border-radius .4rem
+  background rgba(0,0,0,.8)
+  display flex
+  justify-content center
+  align-items center
+  span
+    color #fff
+.fixed
+  position fixed
+  top 0
+  left 0
+  right 0
+  width 100$
+  line-height .88rem
+  text-align center
+  font-size .32rem
+  color #fff
+  background #00bcd4
+  span
+    position absolute
+    left .2rem
+    top .04rem
+    width .8rem
+    height .8rem
+    display flex
+    justify-content center
+    align-items center
+</style>
